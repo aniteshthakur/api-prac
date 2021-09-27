b45ec79862db615efe78dae3a6e8695e2abc7122
aniteshthakur <aniteshthakur@gmail.com>
2021-09-27 21:16:15 +0530

    modify add functionality

--- src/Add.js
+++ src/Add.js
@@ -3,13 +3,15 @@ import React,{useRef} from 'react'
 const Add = () => {
     const number1=useRef();
     const number2=useRef();
-    const onSubmit=()=>{
-        console.log(number1.current.value+number2.current.value)
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        
+        console.log(Number(number1.current.value)+Number(number2.current.value))
     }
     return (
         <form onSubmit={onSubmit}>
-            <input type="number" ref={number1} />
-            <input type="number" ref={number2} />
+            <input type="text" ref={number1} />
+            <input type="text" ref={number2} />
             <button>Add</button>
         </form>
     )
